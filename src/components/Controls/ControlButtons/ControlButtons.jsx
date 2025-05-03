1787dae92fa036ac807640710ef161243c3e30b8
Nuclear303 <oliwier.kotlowski1@gmail.com>
2025-05-03 15:24:14 +0200

    Test with NAS

--- src/components/Controls/ControlButtons/ControlButtons.jsx
+++ src/components/Controls/ControlButtons/ControlButtons.jsx
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ControlButtons.css";
 
 export default function ControlButtons({audioRef}) {
+  const [isPaused, setIsPaused] = useState(false);
+  const [isLooping, setIsLooping] = useState(false);
   const togglePlay = () => {
     if (audioRef.current.paused) {
       audioRef.current.play();
+      setIsPaused(false);
     } else {
       audioRef.current.pause();
+      setIsPaused(true);
     }
   };
 
   const loop = () => {
     audioRef.current.loop = !audioRef.current.loop;
+    setIsLooping(audioRef.current.loop);
   };
+
+  
 
   return (
     <div className="control-buttons">
-      <button className="prev-button">Previous</button>
-      <button className="pause-button" onClick={togglePlay}>Pause</button>
-      <button className="next-button">Next</button>
-      <button className="loop-button" onClick={loop}>Loop</button>
+      <button className="prev-button">
+        <img src="/previous.svg" alt="" />
+      </button>
+      <button className="pause-button" onClick={togglePlay}>
+        <img src={isPaused ? "/play.svg" : "/pause.svg"} alt="" />
+      </button>
+      <button className="next-button">
+        <img src="/next.svg" alt="" />
+        </button>
+      <button className="loop-button" onClick={loop} >
+        <img src="/repeat-1.svg" alt="" className={isLooping ? "looping" : ""}/>
+      </button>
     </div>
   );
 }
